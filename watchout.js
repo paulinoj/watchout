fcbcdfd5e8b6367bd264f9876a1171355396307f
Hack Reactor Students <accounts@hackreactor.com>
2015-05-22 15:46:54 -0700

    Added a draggable player

--- watchout.js
+++ watchout.js
@@ -1,40 +1,38 @@
-var enemies = [];
-var enemy_count = 50;
-
 var Enemy = function(x,y){
   this.x = x;
   this.y = y;
 }
 
+var enemies = [];
+var enemy_count = 10;
+
 var width = 800, height = 500;
 function randX(){
-  return Math.random() * width;
+  return Math.random() * (width-20) + 10;
 }
 function randY(){
-  return Math.random() * height;
+  return Math.random() * (height-20) + 10;
 }
+
 var svg = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height)
-  .append('g')
-  .attr('transform', 'translate(20,20)');
-
+//  .append('g')
+//  .attr('transform', 'translate(40,40)');
 
 for (var i = 0; i < enemy_count; i++) {
   enemies.push(new Enemy(randX(), randY()));
 }
-
 
 function init() {
   svg.selectAll('circle')
     .data(enemies)
     .enter()
     .append('circle')
-    .transition().duration(1000)
     .attr('cx', function(d, i){ return d.x; } )
     .attr('cy', function(d, i){ return d.y; } )
     .attr('r', 10)
-    .style('fill', 'black');
+//    .attr('transform', 'translate(40,40)')
 }
 
 init();
@@ -49,18 +47,32 @@ function update(data){
 
   svg.selectAll('circle')
     .data(data)
-    //.enter()
-    //.append('circle')
     .transition().duration(1000)
     .attr('cx', function(d, i){ return d.x; } )
     .attr('cy', function(d, i){ return d.y; } )
-    //.attr('r', 10)
-    //.style('fill', 'black');
 }
 
-//update(enemies);
 setInterval(function(){update(enemies)},1000);
 
-// setInterval(function(){
+var drag = d3.behavior.drag()
+  .on("drag", function(d, i){
+    d.x += d3.event.dx;
+    d.y += d3.event.dy
+    d3.select(this)
+      .attr('cx', function(d, i){ return d.x})
+      .attr('cy', function(d, i){ return d.y});
+  });
 
-// },1000)
+var player = [{x:width/2, y:height/2}];
+svg.selectAll('.player')
+  .data(player)
+  .enter()
+  .append('circle')
+  .attr('class', 'player')
+  .attr('cx', function(d, i){ return d.x; } )
+  .attr('cy', function(d, i){ return d.y; } )
+  .attr('r', 10)
+  .attr('fill', 'red')
+  .call(drag);
+
+
