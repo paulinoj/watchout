17408c3ee6934d3a49340ef42a3d5ebae7678d9d
Hack Reactor Students <accounts@hackreactor.com>
2015-05-22 14:46:26 -0700

    Added moving enemies

--- watchout.js
+++ watchout.js
@@ -1,2 +1,66 @@
-// start slingin' some d3 here.
+var enemies = [];
+var enemy_count = 50;
 
+var Enemy = function(x,y){
+  this.x = x;
+  this.y = y;
+}
+
+var width = 800, height = 500;
+function randX(){
+  return Math.random() * width;
+}
+function randY(){
+  return Math.random() * height;
+}
+var svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(20,20)');
+
+
+for (var i = 0; i < enemy_count; i++) {
+  enemies.push(new Enemy(randX(), randY()));
+}
+
+
+function init() {
+  svg.selectAll('circle')
+    .data(enemies)
+    .enter()
+    .append('circle')
+    .transition().duration(1000)
+    .attr('cx', function(d, i){ return d.x; } )
+    .attr('cy', function(d, i){ return d.y; } )
+    .attr('r', 10)
+    .style('fill', 'black');
+}
+
+init();
+
+
+
+function update(data){
+  data.forEach(function(item){
+    item.x = randX();
+    item.y = randY();
+  });
+
+  svg.selectAll('circle')
+    .data(data)
+    //.enter()
+    //.append('circle')
+    .transition().duration(1000)
+    .attr('cx', function(d, i){ return d.x; } )
+    .attr('cy', function(d, i){ return d.y; } )
+    //.attr('r', 10)
+    //.style('fill', 'black');
+}
+
+//update(enemies);
+setInterval(function(){update(enemies)},1000);
+
+// setInterval(function(){
+
+// },1000)
